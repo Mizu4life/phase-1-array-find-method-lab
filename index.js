bc47c402f3c37fe8fd2943c5020c4f54afe0c554
Zainab Al-Najjar <eng.zainab.alnajar@gmail.com>
2024-02-25 12:45:33 +0300

    Completed assignment!

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+function superbowlWin(record){
+    let result = record.find(function(element){
+        return (element.result === "W")
+    })
+    if (result != undefined){
+        return result.year;
+    } else {
+        return result;
+    }
+}
+
+// const record = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "W"},
+//     {year: "2016", result: "N/A"}
+//   ];
+
+// console.log(superbowlWin(record));
